40da2fd6bda0636efb007d3c6c61c97305a1ca9c
Guilherme Lepsch <lepsch@gmail.com>
2023-02-05 23:26:42 +0000

    Path section

---
+++ src/pages/About/Path.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import './Path.scss'
+
+function Path () {
+  return <div className='Path'>
+    <div className='title'>
+      <p>Carreira</p>
+    </div>
+    <div className='text-container'>
+      <div className='text'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
+          sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
+          maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
+          aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
+          justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
+          maximus nisl consequat laoreet.</p>
+
+        <p>Cras rhoncus quam dui, a vehicula eros tincidunt ac. Praesent ac tristique ex. Nunc auctor, leo sit amet
+          laoreet tempor, velit nisl volutpat sem, vel consequat nibh elit sit amet ante. Curabitur imperdiet iaculis
+          lectus. Cras id ex eu turpis efficitur condimentum in at ipsum. Nullam diam orci, faucibus in dictum sit amet,
+          ultricies vel nulla. Aenean vel arcu non elit feugiat rutrum. Proin metus nunc, viverra vitae augue at, porta
+          cursus erat. Proin quis felis tellus. Aliquam mollis odio quam, vel iaculis magna commodo feugiat. Nunc
+          imperdiet, nunc a dignissim facilisis, purus purus tincidunt sapien, ac egestas libero odio non lacus. Nam
+          molestie eros sagittis neque rutrum, sed sagittis tellus congue. Maecenas in nisl urna. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras id leo ut eros porta scelerisque a ac
+          urna. Pellentesque in ex tristique, porttitor neque nec, rhoncus libero.</p>
+
+        <p>Sed sed mi justo. Mauris nec iaculis ligula. Donec efficitur bibendum sagittis. Donec nisi justo, consequat
+          vitae venenatis vitae, iaculis eget turpis. Sed a mauris quis nulla congue rutrum ac sit amet massa. Aenean
+          pellentesque tempor magna volutpat mollis. Aenean vitae odio nec sem tempor dictum. Donec at elit feugiat,
+          consequat urna vitae, dapibus magna. Cras non sagittis ante. Mauris condimentum rutrum gravida. Aliquam in
+          lobortis ligula. Phasellus aliquam egestas ipsum ut luctus. Morbi pulvinar est eu feugiat gravida. Vestibulum
+          rhoncus suscipit leo, et faucibus justo hendrerit ut. </p>
+      </div>
+    </div>
+
+  </div>
+}
+
+export default Path
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 import Menu from '../../componets/Menu'
 import Footer from '../../componets/Footer'
-import Cover from './Cover'
 import Social from '../../componets/Social'
 import Schedule from '../../componets/Schedule'
 import WorkPlaces from '../../componets/WorkPlaces'
+import Cover from './Cover'
+import Path from './Path'
 import './index.scss'
 
 function About () {
   return <main className='About'>
     <Menu />
     <Cover />
-
+    <Path />
     <WorkPlaces />
     <Social />
     <Schedule />
