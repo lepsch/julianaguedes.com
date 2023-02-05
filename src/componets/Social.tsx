96f28da173682e0f54c3274d18a321ac8f7c22f7
Guilherme Lepsch <lepsch@gmail.com>
2023-02-05 10:46:18 +0000

    Social section

---
+++ src/componets/Social.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ReactComponent as Instagram } from '../assets/Instagram.svg'
+import './Social.scss';
+
+function Social() {
+  return <div className='Social'>
+    <Instagram />
+    <p>@drajulianaguedes</p>
+  </div>
+}
+
+export default Social;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import Menu from '../../componets/Menu'
 import Footer from '../../componets/Footer'
 import Front from './Front'
 import SelfDisplay from './SelfDisplay'
+import Social from '../../componets/Social'
 import './index.scss'
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
     <Menu />
     <Front />
     <SelfDisplay />
+    <Social />
     <Footer />
   </main>
 }
