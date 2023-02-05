f7da68ff05ddcedfd7eb1c8426b3c26c34ba716f
Guilherme Lepsch <lepsch@gmail.com>
2023-02-05 12:51:21 +0000

    About page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,20 +3,19 @@ import React from 'react'
 import Menu from '../../componets/Menu'
 import Footer from '../../componets/Footer'
 import Front from '../../componets/Front'
-import SelfDisplay from './SelfDisplay'
 import Social from '../../componets/Social'
 import Schedule from '../../componets/Schedule'
 import './index.scss'
 
-function Home() {
-  return <main className='Home'>
+function About() {
+  return <main className='About'>
     <Menu />
     <Front />
-    <SelfDisplay />
+
     <Social />
     <Schedule />
     <Footer />
   </main>
 }
 
-export default Home
+export default About
