6fe4056956b6821d44f840ca745262979423f14e
Guilherme Lepsch <lepsch@gmail.com>
2023-02-05 11:43:00 +0000

    Button theme

--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import classnames from 'classnames'
 import './Button.scss'
 
-const Button: React.FC<{ children?: React.ReactNode | undefined }> = (props) => {
-  return <a className='Button' href='#'>
-    { props.children }
+export enum ButtonTheme {
+  light = 'light',
+  dark = 'dark'
+}
+
+const Button: React.FC<{
+  children?: React.ReactNode | undefined
+  theme?: ButtonTheme
+}> = ({
+  children,
+  theme = ButtonTheme.light
+}) => {
+  return <a className={ classnames('Button', theme) } href='#'>
+    { children }
   </a>
 }
 
