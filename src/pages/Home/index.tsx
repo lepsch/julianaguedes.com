1a51a66ee2bc0624f663b7d55430083072978e29
Guilherme Lepsch <lepsch@gmail.com>
2023-02-25 12:40:49 +0000

    Add other procedures

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,11 @@ import Cover from './Cover'
 import Section from './Section'
 import botox from '../../assets/botox.jpg'
 import acidohialuronico from '../../assets/acido-hialuronico.jpg'
-import fios from '../../assets/fios.jpg'
+import fios from '../../assets/fios-de-pdo.jpg'
 import bioestimuladores from '../../assets/bioestimuladores-de-colageno.jpg'
+import pronokal from '../../assets/pronokal.jpg'
+import peeling from '../../assets/peeling-quimico.jpg'
+import microagulhamento from '../../assets/dermapen-microagulhamento.jpg'
 import './index.scss'
 
 function Home () {
@@ -19,7 +22,7 @@ function Home () {
     <Cover />
     <SelfDisplay />
     <Section
-      name='Botox'
+      name='Toxina Botulínica'
       photo={ botox }
       reverse
       text='O Botox é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
@@ -40,7 +43,7 @@ function Home () {
         maximus nisl consequat laoreet.'
     />
     <Section
-      name='Fios'
+      name='Fios Tensores'
       photo={ fios }
       reverse
       text='Os Fios são um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
@@ -54,6 +57,38 @@ function Home () {
       name='Bioestimuladores de Colágeno'
       photo={ bioestimuladores }
       text='Os Bioestimuladores de Colágeno são um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
+        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
+        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
+        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
+        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
+        maximus nisl consequat laoreet.'
+    />
+    <Section
+      name='PronoKal®'
+      photo={ pronokal }
+      reverse
+      text='O PronoKal® é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
+        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
+        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
+        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
+        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
+        maximus nisl consequat laoreet.'
+    />
+    <Section
+      name='Peeling Químico'
+      photo={ peeling }
+      text='O Peeling é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
+        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
+        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
+        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
+        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
+        maximus nisl consequat laoreet.'
+    />
+    <Section
+      name='Microagulhamento'
+      photo={ microagulhamento }
+      reverse
+      text='O Microagulhamento é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
         sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
         maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
         aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
