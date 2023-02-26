3c8e015f891afa34c3ae8a5f80e5d712034baf4b
Guilherme Lepsch <lepsch@gmail.com>
2023-02-26 20:43:28 +0000

    Text update

--- src/pages/About/Path.tsx
+++ src/pages/About/Path.tsx
@@ -7,29 +7,18 @@ function Path () {
     <h2 className='title'>Carreira</h2>
     <div className='text-container'>
       <div className='text'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-          sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-          maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-          aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-          justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-          maximus nisl consequat laoreet.</p>
+        <p>Médica brasileira formada pela Universidade Federal do Rio de Janeiro - Brasil (UFRJ) (2011).</p>
 
-        <p>Cras rhoncus quam dui, a vehicula eros tincidunt ac. Praesent ac tristique ex. Nunc auctor, leo sit amet
-          laoreet tempor, velit nisl volutpat sem, vel consequat nibh elit sit amet ante. Curabitur imperdiet iaculis
-          lectus. Cras id ex eu turpis efficitur condimentum in at ipsum. Nullam diam orci, faucibus in dictum sit amet,
-          ultricies vel nulla. Aenean vel arcu non elit feugiat rutrum. Proin metus nunc, viverra vitae augue at, porta
-          cursus erat. Proin quis felis tellus. Aliquam mollis odio quam, vel iaculis magna commodo feugiat. Nunc
-          imperdiet, nunc a dignissim facilisis, purus purus tincidunt sapien, ac egestas libero odio non lacus. Nam
-          molestie eros sagittis neque rutrum, sed sagittis tellus congue. Maecenas in nisl urna. Vestibulum ante ipsum
-          primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras id leo ut eros porta scelerisque a ac
-          urna. Pellentesque in ex tristique, porttitor neque nec, rhoncus libero.</p>
+        <p>Especialização em Medicina de Família e Comunidade com 7 anos de atuação em Centros de Saúde da Família</p>
 
-        <p>Sed sed mi justo. Mauris nec iaculis ligula. Donec efficitur bibendum sagittis. Donec nisi justo, consequat
-          vitae venenatis vitae, iaculis eget turpis. Sed a mauris quis nulla congue rutrum ac sit amet massa. Aenean
-          pellentesque tempor magna volutpat mollis. Aenean vitae odio nec sem tempor dictum. Donec at elit feugiat,
-          consequat urna vitae, dapibus magna. Cras non sagittis ante. Mauris condimentum rutrum gravida. Aliquam in
-          lobortis ligula. Phasellus aliquam egestas ipsum ut luctus. Morbi pulvinar est eu feugiat gravida. Vestibulum
-          rhoncus suscipit leo, et faucibus justo hendrerit ut. </p>
+        <p>Pós-graduada em Endocrinologia e Metabologia com Título de Especialista pela Sociedade Brasileira de
+          Endocrinologia e Metabologia (SBEM)</p>
+
+        <p>Mestrado Integrado em Medicina pela Universidade de Lisboa.</p>
+
+        <p>Pós-graduada em Medicina Estética pela Universidad de Alcalá de Henares - ES</p>
+
+        <p>Mestranda em Medicina Estética e Longevidade pela Universidad de Alcalá de Henares - ES</p>
       </div>
     </div>
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,6 +15,7 @@ import pronokal from '../../assets/dra-juliana-guedes-pronokal.jpg'
 import peeling from '../../assets/dra-juliana-guedes-peeling-quimico.jpg'
 import microagulhamento from '../../assets/dra-juliana-guedes-dermapen-microagulhamento.jpg'
 import consultas from '../../assets/dra-juliana-guedes-consultas-medicas.jpg'
+import mesoterapia from '../../assets/dra-juliana-guedes-mesoterapia.jpg'
 import './index.scss'
 
 function Home () {
@@ -23,96 +24,111 @@ function Home () {
     <Cover />
     <SelfDisplay />
     <Section
-      alt='Foto de um paciente com o procedimento de Botox antes e depois'
+      alt='Foto antes e depois de um paciente com o procedimento de toxina botulínica por Dra. Juliana Guedes'
       name='Toxina Botulínica'
       photo={ botox }
       reverse
-      text='O Botox é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='A Toxina Botulínica é uma substância indicada para suavizar as rugas e linhas de expressão do rosto. A
+        toxina funciona como um bloqueador da transmissão neuromuscular, impedindo a transmissão de estímulos dos
+        neurônios para os músculos, de forma que diminua a contração muscular e assim atenue as rugas e linhas de
+        expressão. Seu efeito dura, em média, 120 dias após aplicação.'
     />
     <Section
       alt='Foto da Dra. Juliana Guedes com uma seringa de Ácido Hialurônico'
       name='Ácido Hialurônico'
       photo={ acidohialuronico }
-      text='O Ácido Hialurônico é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='O ácido hialurônico é uma substancia biocompatível em formato de gel. É utilizado amplamente para preencher
+        rugas e sulcos, corrigir pequenas assimetrias e imperfeições e restabelecer o volume perdido no processo de
+        envelhecimento. Além disso tem a capacidade de melhorar o contorno facial e dar volume a estruturas como lábios
+        e glúteos, dentre outras.'
     />
     <Section
       alt='Foto de uma paciente com o procedimento de Fios de PDO aplicados na face pela Dra. Juliana Guedes'
       name='Fios de PDO'
       photo={ fios }
       reverse
-      text='Os Fios são um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='Os Fios de Polidioxanona (PDO) são dispositivos totalmente absorvíveis pelo organismo que têm a capacidade
+        de reduzir a flacidez da pele ao estimular a produção de colágeno de forma permanente pelo período de até 2
+        anos. Além disso, os fios de PDO também podem ser utilizados para efeito trator da pele, promovendo um efeito
+        Lifting não cirúrgico imediato, melhorando o aspeto de uma face “descaída” ou disfarçando uma “papada” causada
+        pela flacidez da pele do pescoço.'
     />
     <Section
       alt='Foto da Dra. Juliana Guedes com os Bioestimuladores de Colágeno'
       name='Bioestimu&#8203;ladores de Colágeno'
       photo={ bioestimuladores }
-      text='Os Bioestimuladores de Colágeno são um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='Os Bioestimuladores são substâncias a base de Ácido poli-L-lático (Sculptra®), Hidroxiapatita de cálcio
+        (Radiesse®) ou Policaprolactona (Ellansé®) que, ao serem injetadas no plano subcutâneo, estimulam a produção de
+        colágeno e hidratam a pele em profundidade, devolvendo o volume na região aplicada, melhorando os sinais de
+        flacidez e envelhecimento da pele (linhas, rugas e dobras da pele). Podem ser usados tanto na face quanto no
+        corpo (pescoço, colo, braços, abdomem, glúteos e pernas). O protocolo de aplicação e número de sessões variam de
+        acordo com o grau de flacidez.'
     />
     <Section
     alt='Foto da Dra. Juliana Guedes com ficha do PronoKal®'
       name='PronoKal®'
       photo={ pronokal }
       reverse
-      text='O PronoKal® é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='O Método PronoKal® é um tratamento médico que combina dieta cetogénica, exercício e coaching. A dieta
+        cetogénica, baixa em hidratos de carbono e gordura, com uma ingestão de proteínas adequada que permitirá que se
+        perca gordura e preserve massa muscular. Dessa forma o paciente perde peso desde os primeiros dias e sente-se
+        com energia, sem passar fome. Há programas específicos para grandes perdas ponderais (PNK) para pacientes com
+        algum grau de obesidade e também para perdas até 5Kg (principalmente gordura localizada) para paciente com IMC
+        (Índice de Massa Corporal) normal.'
     />
     <Section
       alt='Foto de uma paciente com o procedimento de Peeling Químico'
       name='Peeling Químico'
       photo={ peeling }
-      text='O Peeling é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='O peeling químico é um tratamento à pele que remove suas camadas mais superficiais, promovendo o processo de
+        renovação celular através da deposição de ativos específicos sob a pele. É capaz de normalizar a pigmentação da
+        pele, atenuar marcas e reduzir rugas, tornando a pele mais lisa, luminosa e com aspeto jovial.
+
+        Ideal para tratamento de discromias (lêntigos, manchas, melasma, olheiras pigmentadas), acne ativa e cicatrizes
+        de acne, poros dilatados e reduzir rugas superficiais e profundas.
+      '
     />
     <Section
       alt='Foto de uma paciente com o procedimento de Microagulhamento'
       name='Microneedling'
       photo={ microagulhamento }
       reverse
-      text='O Microagulhamento é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='O Microagulhamento é um tratamento estético que serve para atenuar cicatrizes de acne, disfarçar manchas, ou
+        outras cicatrizes, rugas ou linhas de expressão da pele. Tal é feito através de uma estimulação natural tendo
+        por base micro-agulhas que penetram na derme favorecendo a formação de novas fibras de colágeno, que dão firmeza
+        e sustentação à pele.
+
+        Associados ao microagulhamento, podem-se utilizar uma série de ativos para revitalização da pele, reduzir poros
+        dilatados, clarear manchas, promover hidratação profunda e revitalização capilar (tratamento de alopecias).
+
+        São realizadas sessões com intervalo mínimo de 21 dias. O número de sessões varia de acordo com o tipo de pele
+        e o efeito pretendido.
+      '
     />
     <Section
       alt='Foto da Dra. Juliana Guedes na Clínica Senhora-a-Branca'
       name='Consultas Médicas'
       photo={ consultas }
-      text='O Microagulhamento é um procedimento estético que atua na diminuição das rugas e linhas de expressão, além de amenizar a flacidez da pele. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla erat erat, ut congue purus congue
-        sed. Quisque pharetra dui nisi. Sed scelerisque dui vel massa ornare condimentum. Praesent vestibulum enim
-        maximus justo tristique molestie sit amet non leo. Mauris gravida metus eros, nec porta lacus pulvinar
-        aliquam. Nullam convallis massa ac eros laoreet, eget iaculis purus ullamcorper. Vivamus congue scelerisque
-        justo, in faucibus purus pretium id. Nullam condimentum consequat eros rhoncus porta. Curabitur sit amet enim
-        maximus nisl consequat laoreet.'
+      text='Consultas médicas em Clínica Geral, consulta e tratamento de obesidade, diabetes, alterações do colesterol
+        (dislispidemias), doenças da tireoide, alterações hormonais (Ovários poliquisticos, menopausa, andropausa),
+        check up clínico, promoção de qualidade de vida e bem estar.'
+    />
+    <Section
+      alt='Foto da Dra. Juliana Guedes na Clínica Senhora-a-Branca'
+      name='Mesoterapia'
+      photo={ mesoterapia }
+      reverse
+      text='É um método minimamente invasivo, que consiste em micro injeções com a finalidade de introduzir o produto
+        adequado com doses mínimas e o mais próximo possível da área a ser tratada, com baixa absorção sistêmica
+        (evitando desta forma a passagem pelo sistema sanguíneo).
+
+        O produto aplicado é escolhido pelo médico de acordo com o objetivo do tratamento, podendo ser utilizadas
+        substâncias isoladas ou associadas em cocktails de vitaminas, nutrientes, aminoácidos, substâncias lipolíticas e
+        refirmantes, extratos de enzimas e plantas, etc. A técnica é indicada para homens e mulheres e utilizada para o
+        tratamento de: celulite, gordura localizada, redução da papada, estrias, flacidez, revitalização da pele.
+
+        As áreas geralmente tratadas são: peito, região interna dos braços, mãos, abdômen, flancos, costas, região
+        interna e posterior da coxa e glúteo.'
     />
     <Social />
     <Schedule />
