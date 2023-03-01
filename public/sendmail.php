fab54519cbf77e40f846ce939d5ff0e682acb9fe
Guilherme Lepsch <lepsch@gmail.com>
2023-03-01 12:58:03 +0000

    Fix email from / to

--- public/sendmail.php
+++ public/sendmail.php
@@ -3,7 +3,7 @@
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
-$to = '"Dra. Juliana Guedes" <dra@julianaguedes.com>';
+$to = $data->email;
 $subject = 'Mensagem enviada (www.julianaguedes.com)';
 $message = 'Olá. Obrigada por entrar em contato!' . "\r\n" . "\r\n" .
     'A sua mensagem foi recebida e será respondida o mais breve possível.' . "\r\n" . "\r\n" .
@@ -16,7 +16,7 @@ $message = 'Olá. Obrigada por entrar em contato!' . "\r\n" . "\r\n" .
     '> Mensagem: ' . $data->mensagem . "\r\n";
 $headers = 'From: "Dra. Juliana Guedes" <dra@julianaguedes.com>' . "\r\n" .
     'Reply-To: "Dra. Juliana Guedes" <dra@julianaguedes.com>' . "\r\n" .
-    'Cc: ' . $data->email . "\r\n" .
+    'Bcc: "Dra. Juliana Guedes" <dra@julianaguedes.com>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
