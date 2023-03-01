3bd99f75f78ac3124c1be0070c2f56b83c42e19c
Guilherme Lepsch <lepsch@gmail.com>
2023-03-01 16:48:24 +0000

    NoScript capable

--- public/sendmail.php
+++ public/sendmail.php
@@ -1,13 +1,21 @@
 <?php
-// TODO: Check if the data come as form data or JSON
-$json = file_get_contents('php://input');
-$data = json_decode($json);
+$contentType =  $_SERVER["CONTENT_TYPE"];
+if ($contentType === 'application/json') {
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+} else {
+    $data = new stdClass();
+    $data->nome = $_POST["nome"];
+    $data->email = $_POST["email"];
+    $data->telefone = $_POST["telefone"];
+    $data->mensagem = $_POST["mensagem"];
+}
 
 $to = $data->email;
 $subject = 'Mensagem enviada (www.julianaguedes.com)';
 $message = 'Olá. Obrigada por entrar em contato!' . "\r\n" . "\r\n" .
     'A sua mensagem foi recebida e será respondida o mais breve possível.' . "\r\n" . "\r\n" .
-    'Até breve!' . "\r\n" .
+    'Até logo!' . "\r\n" .
     'Dra. Juliana Guedes' . "\r\n" . "\r\n" .
     '> Mensagem enviada:' . "\r\n" .
     '> Nome: ' . $data->nome . "\r\n" .
@@ -20,4 +28,8 @@ $headers = 'From: "Dra. Juliana Guedes" <dra@julianaguedes.com>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
+if ($contentType !== 'application/json') {
+    header('Location: https://www.julianaguedes.com/contacto', true, 301);
+    exit;
+}
 ?>
