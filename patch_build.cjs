7306e4b49ba61df9a4a51dff4aaae367340bb1e8
Guilherme Lepsch <lepsch@gmail.com>
2024-07-06 01:24:46 +0100

    Update htaccess
    1. Prepend `www` and redirect;
    2. Disable trailing slash redirects;
    3. Rewrite directories to `index.php`;

--- patch_build.cjs
+++ patch_build.cjs
@@ -6,13 +6,6 @@ const { glob } = require('glob')
 const PHP = `<?php
 session_set_cookie_params(["SameSite" => "Strict"]);
 session_start();
-
-$host = $_SERVER['HTTP_HOST'];
-if (substr($host, 0, 4) !== 'www.') {
-  $host = 'www.' . $host;
-  header('Location: https://' . $host . $_SERVER['REQUEST_URI'], true, 301);
-  exit;
-}
 ?>
 `
 
