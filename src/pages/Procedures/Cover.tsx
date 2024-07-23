aa59ff937fabddfd4349fd8bb591e4c03c3a415f
Guilherme Lepsch <lepsch@gmail.com>
2024-07-23 12:48:33 +0100

    Add procedures navigation aria

--- src/pages/Procedures/Cover.tsx
+++ src/pages/Procedures/Cover.tsx
@@ -3,18 +3,18 @@ import styles from './Cover.module.scss'
 export function Cover() {
   return (
     <header className={styles['Cover']}>
-      <nav className={styles['title-container']} aria-label='tratamentos-title'>
+      <nav className={styles['title-container']} aria-labelledby='tratamentos-title'>
         <h1 id='tratamentos-title' className={styles['title']}>Tratamentos</h1>
-        <ul>
-          <li><a href='#acido-hialuronico'>Ácido Hialurónico</a></li>
-          <li><a href='#toxina-botulinica'>Toxina Botulínica</a></li>
-          <li><a href='#bioestimuladores-de-colageno'>Bioestimuladores de Colagénio</a></li>
-          <li><a href='#fios-de-pdo'>Fios de PDO</a></li>
-          <li><a href='#pronokal'>PronoKal®</a></li>
-          <li><a href='#peeling-quimico'>Peeling Químico</a></li>
-          <li><a href='#microneedling'>Microneedling</a></li>
-          <li><a href='#consultas-medicas'>Consultas Médicas</a></li>
-          <li><a href='#mesoterapia'>Mesoterapia</a></li>
+        <ul role='menu'>
+          <li role='none'><a role='menuitem' href='#acido-hialuronico'>Ácido Hialurónico</a></li>
+          <li role='none'><a role='menuitem' href='#toxina-botulinica'>Toxina Botulínica</a></li>
+          <li role='none'><a role='menuitem' href='#bioestimuladores-de-colageno'>Bioestimuladores de Colagénio</a></li>
+          <li role='none'><a role='menuitem' href='#fios-de-pdo'>Fios de PDO</a></li>
+          <li role='none'><a role='menuitem' href='#pronokal'>PronoKal®</a></li>
+          <li role='none'><a role='menuitem' href='#peeling-quimico'>Peeling Químico</a></li>
+          <li role='none'><a role='menuitem' href='#microneedling'>Microneedling</a></li>
+          <li role='none'><a role='menuitem' href='#consultas-medicas'>Consultas Médicas</a></li>
+          <li role='none'><a role='menuitem' href='#mesoterapia'>Mesoterapia</a></li>
         </ul>
       </nav>
     </header>
